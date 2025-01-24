cf8f885c615762119056076da7ffc6313236a6d6
shanta1996 <shantachaudhary490@gmail.com>
2025-01-24 17:38:24 +0545

    changes

--- src/pages/ThirdPage.jsx
+++ src/pages/ThirdPage.jsx
@@ -8,9 +8,15 @@ import gsap from 'gsap'
 
 
 const ThirdPage = () => {
-    const imgRef=useRef(null)
-    useGSAP(()=>{
-        gsap.to('')
+    useGSAP(() => {
+        gsap.to('.roundBox:nth-child(odd)', {
+            transform:'translateY(-100px)',
+            // opacity:0
+        })
+        gsap.to('.roundBox:nth-child(even)', {
+            transform:'translateY(100px)',
+            // opacity:0
+        })
     })
     return (
         <>
@@ -22,21 +28,27 @@ const ThirdPage = () => {
                         </div>
                         <div className='ornamentSlide h-screen w-full bg-yellow-500 flex-shrink-0 flex items-center justify-center gap-[80px] relative'>
                             {ornamentData.map((getData, index) => (
-                                <Ornaments imgRef={imgRef} key={index} getData={getData} />
+                                <div className=' roundBox'  key={index}>
+                                    <div className=' h-[200px] w-[200px] rounded-full overflow-hidden'>
+                                        <img src={getData.image} className='h-full w-full object-cover bg-center' alt="" />
+                                    </div>
+                                    <h4 className='text-xl text-center capitalize mt-2'>{getData.title}</h4>
+                                </div>
+                                // <Ornaments imgRef={getData.imgRef} key={index} getData={getData} />
                             ))}
                         </div>
-                        <div className='ornamentSlide h-screen w-full bg-yellow-200 flex justify-center items-center flex-shrink-0'>
+                        {/* <div className='ornamentSlide h-screen w-full bg-yellow-200 flex justify-center items-center flex-shrink-0'>
                             <h2 className='text-6xl font-[Ubuntu] font-medium '>Tharu Cultures</h2>
                         </div>
                         <div className='ornamentSlide h-screen w-full bg-green-200 flex-shrink-0 flex items-center  gap-[80px] relative'>
                             {
                                 culture.map((image,i)=>(
-                                   <Dhakiya imgRef={imgRef} key={i} image={image} />
+                                   <Dhakiya imgRef={image.imgRef} key={i} image={image} />
                                 ))
                             }
                            
                             
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
