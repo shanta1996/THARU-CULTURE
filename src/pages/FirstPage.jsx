d042e23a3a474ae0a51c3436142fd57847b5645c
shanta1996 <shantachaudhary490@gmail.com>
2025-01-25 15:45:31 +0545

    changes

--- src/pages/FirstPage.jsx
+++ src/pages/FirstPage.jsx
@@ -2,38 +2,32 @@ import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import React, { useRef, useState } from 'react'
 import TiltedText from '../components/TiltedText'
+import { ScrollTrigger } from 'gsap/all'
 
+
+gsap.registerPlugin(ScrollTrigger)
 const FirstPage = () => {
-    const [valX, setValX] = useState(0)
-    const [valY, setValY] = useState(0)
     const tiltedText = useRef(null)
-    const handleMousMove = (e) => {
-        setValX((e.clientX - tiltedText.current.getBoundingClientRect().x - tiltedText.current.getBoundingClientRect().width / 2) / 60)
-        setValY(-(e.clientY/30))
-
-        // setValY((e.clientY - tiltedText.current.getBoundingClientRect().y - tiltedText.current.getBoundingClientRect().hieght / 2)/50)
-        // tiltedText.current.style.transform = `rotateX(${valY}deg) rotateY(${valX}deg)`
-
-    }
-    
-    useGSAP(()=>{
-        gsap.to(tiltedText.current,{
-            transform:`rotateX(${valY}deg) rotateY(${valX}deg)`,
-            duration:5,
-            ease: "power1.out",
-        })
-        // gsap.to('.firstPage',{
-        //     clip:'0%'
-        // })
-    },[valX,valY])
+  useGSAP(()=>{
+    gsap.to('.tiltText',{
+        clipPath: 'circle(15% at 50% 50%)',
+        scrollTrigger:{
+            trigger:'.tiltText',
+            markers:true,
+            scrub:1,
+            start:'top top',
+            // end:'bottom bottom',
+        }
+    })
+  })
     return (
         <>
-            <div className=' w-full relative'>
-                <div className='h-screen text-9xl font-["Ubuntu"] font-bold flex items-center justify-center'>Scroll to Explore</div>
-                <div onMouseMove={(e) => {
-                    handleMousMove(e)
-                }} style={{clipPath:'circle(100% at 50% 50%)'}} className='firstPage absolute h-screen w-full top-0 bg-zinc-100 flex items-center justify-center'>
-                    <TiltedText tilt={tiltedText} />
+            <div className='page1 w-full relative'>
+                <div className='firstPage h-screen text-9xl font-["Ubuntu"] font-bold flex items-center justify-center '>
+                    <div className='bg-red-300 scale-[1.3] '>Scroll to Explore</div>
+                </div>
+                <div style={{clipPath:'circle(100% at 50% 50%)'}} className='tiltText absolute h-screen w-full top-0 bg-zinc-100 flex items-center justify-center'>
+                    <TiltedText tilt={tiltedText}/>
                 </div>
 
             </div>
