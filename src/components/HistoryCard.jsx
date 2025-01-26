5299b0c20d68c3b57d137b30759f523bf51983b9
shanta1996 <shantachaudhary490@gmail.com>
2025-01-26 17:30:02 +0545

    changes

--- src/components/HistoryCard.jsx
+++ src/components/HistoryCard.jsx
@@ -1,51 +1,16 @@
-import { useGSAP } from '@gsap/react'
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/all'
+
 import React from 'react'
 
-gsap.registerPlugin(ScrollTrigger)
 const HistoryCard = ({getData}) => {
-    var tl=gsap.timeline()
-    useGSAP(()=>{
-        // gsap.set('.text',{
-        //     opacity:0.1
-        // })
-        tl.to('.textHeading',{
-            opacity:1,
-            ease:'power1',
-            scrollTrigger:{
-                trigger:'.textHeading',
-                // markers:true,
-                start:'top 50%',
-                end:'top 60%',
-                scrub:1,
-            }
-        })
-        tl.from('.textPara',{
-            x:-100,
-            ease:'power4',
-            stagger:0.03,
-            scrollTrigger:{
-                trigger:'.card',
-                markers:true,
-                start:'top 60%',
-                end:'bottom bottom',
-                scrub:0.01,
-            }
-        })
-    })
+ 
     const{title,description}=getData
     return (
         <>
             <div className='card w-[90%] p-3 border rounded-2xl overflow-hidden duration-500 ease-linear m-5'>
                 <h3 className='text textHeading text-2xl font-semibold my-2'>{title}</h3>
-                {/* {title.split('').map((heading,i)=>(
-                    <span className='text-2xl font-semibold inline-block' key={i}>{heading}</span>
-                ))} */}
-                {/* <p className='text-md md:text-lg 2xl:text-xl mt-5 '>{description}</p> */}
-                {description.split("").map((description,i)=>(
-                    <span className='text textPara text-md md:text-lg 2xl:text-xl mt-5 ' key={i}>{description}</span>
-                ))}
+              
+                <p className='text-md md:text-lg 2xl:text-xl mt-5 '>{description}</p>
+              
             </div>
         </>
     )
--- src/components/HistoryHeading.jsx
+++ src/components/HistoryHeading.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+import React, { useState } from 'react'
 
+gsap.registerPlugin(ScrollTrigger)
 const HistoryHeading = () => {
+    const [textPara, setTextPara] = useState('The Tharu people of Nepal have a long history marked by resilience and adaptability. From their ancient roots in the Terai region to their struggle for recognition in modern Nepal, the Tharu community has played an important role in the cultural and social fabric of the country. Despite facing challenges like land displacement, cultural marginalization, and political exclusion, the Tharus continue to assert their identity, rights, and cultural heritage in the modern political landscape of Nepal.')
+
+    const letters=textPara.split('')
+    console.log(letters);
+    useGSAP(()=>{
+        gsap.set('.para span',{
+            opacity:0.1
+        })
+        gsap.to('.para span',{
+            opacity:1,
+            stagger:0.01,
+            ease:'power2',
+            scrollTrigger:{
+                trigger:'.mainDiv',
+                markers:true,
+                start:'top 50%',
+                scrub:1
+            }
+        })
+    })
+    
     return (
         <>
-            <div className='sticky top-0 left-0 w-[45%] p-5'>
+            <div className='sticky mainDiv  top-0 left-0 w-[45%] p-5'>
                 <h1 className='capitalize text-2xl md:text-4xl 2xl:text-5xl'>history of tharu in nepal</h1>
-                <p className='text-md md:text-lg 2xl:text-xl mt-5'>The Tharu people of Nepal have a long history marked by resilience and adaptability. From their ancient roots in the Terai region to their struggle for recognition in modern Nepal, the Tharu community has played an important role in the cultural and social fabric of the country. Despite facing challenges like land displacement, cultural marginalization, and political exclusion, the Tharus continue to assert their identity, rights, and cultural heritage in the modern political landscape of Nepal.</p>
+                <p className='para text-md md:text-lg 2xl:text-xl mt-5'>{letters.map((text,i)=>(
+                    <span key={i}>{text}</span>
+                ))}</p>
             </div>
         </>
     )
