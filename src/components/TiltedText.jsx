2d79828c8b4fe10facd83a8ae8121038faff83ac
shanta1996 <shantachaudhary490@gmail.com>
2025-01-26 17:47:25 +0545

    changes

--- src/components/TiltedText.jsx
+++ src/components/TiltedText.jsx
@@ -1,10 +1,20 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 import React from 'react'
 
 const TiltedText = ({tilt}) => {
+  useGSAP(()=>{
+    gsap.from('.tilt',{
+      transform:'rotateX(-150deg)',
+      opacity:0,
+      duration:1,
+      // ease:'power1',
+    })
+  })
   return (
     <>
-        <div  ref={tilt} className='rotate-x-[30deg] font-["Ubuntu"] font-bold relative'>
-                        <h1 className='text-9xl tracking-tight '>THARU CULTURE</h1>
+        <div ref={tilt} className='rotate-x-[30deg] font-["Ubuntu"] font-bold relative'>
+                        <h1 className='tilt text-9xl tracking-tight '>THARU CULTURE</h1>
                         <div className='h-[60px] w-[60px] overflow-hidden rounded-full absolute top-1/2 -translate-y-[95px] left-0 -translate-x-1/2 '>
                             <img src="/sirbindi.png" className='object-cover' alt="" />
                         </div>
