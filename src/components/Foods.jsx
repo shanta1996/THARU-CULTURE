179b4bbb4cbb44fe6be3c6065f7518f98d3de018
shanta1996 <shantachaudhary490@gmail.com>
2025-01-27 21:16:51 +0545

    changes

--- src/components/Foods.jsx
+++ src/components/Foods.jsx
@@ -5,7 +5,7 @@ const Foods = () => {
   
     return (
         <div className=' bg-[#B5C18E] w-full h-full flex items-center justify-center relative'>
-            <p className='text-5xl md:text-9xl tracking-tight md:leading-none leading-16 text-center font-medium font-[ubuntu] mt-11 md:mt-auto py-7 md:py-[60px] 2xl:py-[100px]  bg-transparent md:bg-[#DEAC80]  absolute md:bottom-0 bottom-auto md:top-auto top-0 w-full uppercase md:border-none border-b-[1px] border-t-[1px]  border-zinc-900'>tharu Foods</p>
+            <p className='text-5xl md:text-9xl tracking-tight md:leading-none leading-16 text-center font-medium font-[ubuntu] mt-11 md:mt-auto py-7 md:py-[60px] 2xl:py-[100px]  bg-transparent md:bg-[#DEAC80]  absolute md:bottom-0 bottom-auto md:top-auto top-0 w-full uppercase '>tharu Foods</p>
             <div className='grid grid-cols-2 md:grid-cols-6 gap-[10px] md:gap-[30px] 2xl:gap-[60px] justify-items-center align-top md:px-2 md:mt-0 mt-[150px]  2xl:px-10 px-5'>
             {
                 foodData.map((foodImg, i) => (
